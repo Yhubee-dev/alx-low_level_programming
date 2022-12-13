d5de05fbbb20bf48eb877a218f8a2ee7db81ba69
Ubohotoabasi Akpan <akpan.uboho@outlook.com>
2022-12-12 20:33:49 -0800

    multiples

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdiio.h>
+
+/**
+ * main - print lowercase a-z
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+	
